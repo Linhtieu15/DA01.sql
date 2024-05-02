e9ab82c3bbbdc5b41f42a66ddbd8738c6a7936ca
Linhtieu15 <168364889+Linhtieu15@users.noreply.github.com>
2024-05-03 05:31:25 +0700

    Practice02.sql

--- Practice02.sql
+++ Practice02.sql
@@ -24,3 +24,41 @@ FROM posts
 WHERE post_date >= '2021/01/01' AND post_date <= '2021/12/31'
 GROUP BY user_id
 HAVING COUNT(post_id)>= 2 ;
+
+--Ex 7:
+SELECT card_name, max(issued_amount) - min(issued_amount) as difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY card_name DESC;
+
+--Ex 8:
+SELECT manufacturer,
+count (drug) as drug_count,
+abs (sum(cogs-total_sales)) as total_loss
+FROM pharmacy_sales
+WHERE (cogs-total_sales) > 0
+group by manufacturer  
+order by abs (sum(cogs-total_sales)) DESC;
+
+--Ex  9:
+select id,movie, description, rating
+from Cinema
+where id%2 <> 0 and description <> 'boring'
+order by rating desc 
+
+--Ex 10:
+ select teacher_id, count(distinct subject_id) as cnt
+ from Teacher
+ group by teacher_id;
+
+--Ex 11:
+select user_id, count(follower_id) as followers_count
+from Followers
+group by user_id
+order by user_id asc ;
+
+--Ex 12:
+select class from Courses
+group by class
+having count(student) >= 5;
+
