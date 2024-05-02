87cbc8089c2e1b64fa21399526bcfff3eeb26c71
Linhtieu15 <168364889+Linhtieu15@users.noreply.github.com>
2024-05-03 04:32:53 +0700

    Update Practice02.sql

--- Practice02.sql
+++ Practice02.sql
@@ -1,1 +1,26 @@
+--Ex 1:
+SELECT DISTINCT CITY FROM STATION
+WHERE ID%2=0;
 
+--Ex 2:
+select count (CITY)  - count (distinct CITY) from STATION;
+
+--Ex 4:
+SELECT round(CAST (SUM(item_count*order_occurrences)/SUM(order_occurrences) 
+AS DECIMAL),1) as mean
+FROM items_per_order;
+-- CAST XXX AS DECIMAL: chuyển từ int về decimal do Postgresql coi kết quả chia là số nguyên, k thể round up
+
+-- Ex 5:
+SELECT DISTINCT candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING count(skill) = 3;
+
+--Ex 6:
+SELECT user_id, MAX(Date(post_date)) - MIN (Date(post_date))	AS days_between
+FROM posts
+WHERE post_date >= '2021/01/01' AND post_date <= '2021/12/31'
+GROUP BY user_id
+HAVING COUNT(post_id)>= 2 ;
